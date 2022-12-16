29aecefcfac8c78a4163791216eb41a86d8cb417
PC <PC@10.188.104.7>
2022-12-16 13:06:38 +0100

    v1.-proyecto creado

---
+++ CalcularPI/src/main/java/es/calcularpi/CalcularPI.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.calcularpi;
+
+/**
+ *
+ * @author PC
+ */
+public class CalcPI {
+
+     public static void main(String[] args) {
+        MathContext mc = new MathContext(10);         
+        //meter en el método compute
+        BigDecimal pi = new BigDecimal(0);
+        BigDecimal limit = new BigDecimal(1).movePointLeft(10);
+        boolean stop = false;
+        for (int k = 0; !stop; k++) {
+            //meter en el método piFunction  
+            int k8 = 8 * k;
+            BigDecimal val1 = new BigDecimal(4);
+            val1 = val1.divide(new BigDecimal(k8 + 1), mc);
+            BigDecimal val2 = new BigDecimal(-2);
+            val2 = val2.divide(new BigDecimal(k8 + 4), mc);
+            BigDecimal val3 = new BigDecimal(-1);
+            val3 = val3.divide(new BigDecimal(k8 + 5), mc);
+            BigDecimal val4 = new BigDecimal(-1);
+            val4 = val4.divide(new BigDecimal(k8 + 6), mc);
+            BigDecimal val = val1;
+            val = val.add(val2);
+            val = val.add(val3);
+            val = val.add(val4);
+            BigDecimal multiplier = new BigDecimal(16);
+            multiplier = multiplier.pow(k);
+            BigDecimal one = new BigDecimal(1);
+            multiplier = one.divide(multiplier, mc);
+            val = val.multiply(multiplier);
+            BigDecimal piK = val;
+            //meter en el método piFunction                
+            pi = pi.add(piK);
+            if (piK.compareTo(limit) < 0) {
+                stop = true;
+            }
+        }
+        System.out.println(pi.round(mc));
+        //meter en el método compute
+    }
+     
+}
